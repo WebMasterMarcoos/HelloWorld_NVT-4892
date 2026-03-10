03fad942801c559fee8cd730d0c15142dec493c5
WebMasterMarcoos <d0ris.d0rae.d0ra@gmail.com>
2026-03-10 14:22:57 +0800

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+// This program outputs the message "Hello World!" to the monitor
+// Created by Marcus Webb - NVT-4892
+
+#include <iostream>
+
+int main() {
+
+   std::cout << "Hello World!\n";
+
+   return 0;
+
+}
